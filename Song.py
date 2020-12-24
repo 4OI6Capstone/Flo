5ec0b6c240bf6619ecfc0d6643fdc4b56a51ee7e
zhangc97 <zhangc16@mcmaster.ca>
2020-12-24 17:27:25 -0500

    Boilerplate flask program

---
+++ Song.py
@@ -0,0 +1,9 @@
+class Song:
+    title = None
+    album = None
+    artist = None
+
+    def __init__(self, title, album, artist):
+        self.title = title
+        self.album = album
+        self.artist = artist
