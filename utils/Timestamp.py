02e2fc0c7d04560bf4be6cb8edf89e898bcd9b88
sinananc <43153358+sinananc@users.noreply.github.com>
2021-03-05 14:45:53 -0500

    Adding timestamp function

--- utils/Timestamp.py
+++ utils/Timestamp.py
@@ -5,68 +5,17 @@ from pydub import AudioSegment
 from pydub.utils import which
 import time
 from random import randint
-from utils.songs import Song
-
-
-def find_best_sync_point(bottom_file_beats, top_file_beats, max_mix_sample, offset, mode):
-
-    offset = offset
-    matches_per_round = []
-
-    # turning args to numpy arrays
-    bottom_file_beats = np.array(bottom_file_beats)
-    top_file_beats = np.array(top_file_beats)
-
-    for rn in range(bottom_file_beats.shape[0]):
-
-        try:
-
-            zero_sync_samples = bottom_file_beats[rn] - top_file_beats[0]
-            slider = top_file_beats + (zero_sync_samples)
-
-            for i in range(len(slider)):
-                if slider[i] <= max_mix_sample:
-                    continue
-                else:
-                    slider[i] = slider[i] - max_mix_sample
-
-            matches = []
-            tested_beat_index = 0
-            all_sample_beats = np.concatenate((slider, bottom_file_beats))
-            all_sample_beats.sort()
-
-            for i in range (1, all_sample_beats.shape[0]):
-                if all_sample_beats[i] == all_sample_beats[tested_beat_index] or abs(all_sample_beats[i] - all_sample_beats[tested_beat_index]) <= offset:
-                    matches.append(all_sample_beats[i])
-                    matches.append(all_sample_beats[tested_beat_index])
-                    tested_beat_index+=1
-
-                else:
-                    tested_beat_index+=1
-
-            matches_per_round.append(len(matches)/2/len(top_file_beats))
-
-        except Exception as err:
-            # exc_type, exc_value, exc_traceback = sys.exc_info()
-            # print ('\t🔎  We had a sliding window problem :|\n\tError message: {}\n\tError on file: {}\n\tError on line: {}\n'.format(err, exc_traceback.tb_frame.f_code.co_filename, exc_traceback.tb_lineno))
-            matches_per_round.append(0)
-
-    if mode == 'first':
-        sync_beat_number = np.argmax(matches_per_round)
-
-    else: # random (default)
-        sync_beat_number = np.random.choice(np.argwhere(matches_per_round == np.amax(matches_per_round)).reshape(-1,))
-
-    sync_sample = bottom_file_beats[sync_beat_number] - top_file_beats[0]
-    sync_beat_accuracy = np.max(matches_per_round)
-
-    return sync_sample, sync_beat_number, sync_beat_accuracy
+from songs import Song
+import Timestamp_utils as tu
 
 
 
 
 
-def timestamp(top_file, bottom_file, sr=22050, mix_mode='random', offset=880, trim_silence=False, sync_sample=None, timestamp=None):
+
+
+#top_file represents the first music file that's being mixed and bottom_file represents the second song being brought in
+def get_timestamp(top_file, bottom_file, sr=22050, mix_mode='random', offset=880, trim_silence=False, sync_sample=None, timestamp=None):
 
     # loading the first song in the transition
     y_top_file, sr = librosa.load(top_file, sr=sr)
@@ -101,7 +50,7 @@ def timestamp(top_file, bottom_file, sr=22050, mix_mode='random', offset=880, tr
     bottom_file_data = audioowl.analyze_samples(y_bottom_file, sr)
 
     # find mixing point
-    sync_sample, sync_beat_number, sync_beat_accuracy = find_best_sync_point(
+    sync_sample, sync_beat_number, sync_beat_accuracy = tu.find_best_sync_point(
                                                         top_file_beats=top_file_data['beat_samples'],
                                                         bottom_file_beats=bottom_file_data['beat_samples'],
                                                         max_mix_sample=y_bottom_file.shape[0],
@@ -115,7 +64,7 @@ def timestamp(top_file, bottom_file, sr=22050, mix_mode='random', offset=880, tr
 
     return sync_time_ms
 
-def timestamp_loop(top_file, bottom_file, sr=22050, mix_mode='random', offset=880, trim_silence=False, sync_sample=None, timestamp=None):
+def get_timestamp_loop(top_file, bottom_file, sr=22050, mix_mode='random', offset=880, trim_silence=False, sync_sample=None, timestamp=None):
 
     # loading top file
     y_top_file, sr = librosa.load(top_file, sr=sr)
@@ -151,7 +100,7 @@ def timestamp_loop(top_file, bottom_file, sr=22050, mix_mode='random', offset=88
     bottom_file_data = audioowl.analyze_samples(y_bottom_file, sr)
 
     # find mixing point
-    sync_sample, sync_beat_number, sync_beat_accuracy = find_best_sync_point(
+    sync_sample, sync_beat_number, sync_beat_accuracy = tu.find_best_sync_point(
                                                         top_file_beats=top_file_data['beat_samples'],
                                                         bottom_file_beats=bottom_file_data['beat_samples'],
                                                         max_mix_sample=y_bottom_file.shape[0],
