2b7bc7392d5d7777491f1ae4dd908714e1902de1
sinananc <43153358+sinananc@users.noreply.github.com>
2021-03-21 14:25:26 -0400

    Adding timestamp

--- utils/Timestamp.py
+++ utils/Timestamp.py
@@ -1,25 +1,23 @@
 import audioowl
 import librosa
 import numpy as np
-from pydub import AudioSegment
-from pydub.utils import which
-import time
-from random import randint
-from songs import Song
-import Timestamp_utils as tu
+import utils.timestamp_utils as tu
 
 
-
-
-
-s1 = Song()
-s2 = Song()
-
-#top_file represents the first music file that's being mixed and bottom_file represents the second song being brought in
-def get_timestamp(s1, s2, sr=22050, mix_mode='random', offset=880, trim_silence=False):
-
+def get_timestamp(prev_song, next_song, sr=22050, mix_mode='random', offset=880, trim_silence=False):
+    """
+        :param next_song: next song in the mix
+        :param prev_song: previous song
+        :param sr: sample rate
+        :param mix_mode:
+        :param offset:
+        :param trim_silence:
+        :return: timestamp in milliseconds where the best place to transition is
+    """
     # loading the first song in the transition
-    y_top_file, sr = librosa.load(s1.filename, sr=sr)
+    top_file = next_song.filename
+    bottom_file = prev_song.filename
+    y_top_file, sr = librosa.load(top_file, sr=sr)
 
     if trim_silence:
         try:
@@ -31,12 +29,12 @@ def get_timestamp(s1, s2, sr=22050, mix_mode='random', offset=880, trim_silence=
             pass
 
     # loading second song in the transition
-    y_bottom_file, sr = librosa.load(s2.filename, sr=sr)
+    y_bottom_file, sr = librosa.load(bottom_file, sr=sr)
 
     # checking if the durations allow proper mixing
     y_bottom_file_repetitions = 1
-    while (y_bottom_file.shape[0] * y_bottom_file_repetitions) < y_top_file.shape[0] :
-        y_bottom_file_repetitions+=1
+    while (y_bottom_file.shape[0] * y_bottom_file_repetitions) < y_top_file.shape[0]:
+        y_bottom_file_repetitions += 1
 
     # repeating y_bottom_file (second song in transition) if needed
     if (y_bottom_file_repetitions > 1):
@@ -45,30 +43,38 @@ def get_timestamp(s1, s2, sr=22050, mix_mode='random', offset=880, trim_silence=
             y_bottom_file_duplications.append(y_bottom_file)
         y_bottom_file = np.hstack((y_bottom_file_duplications))
 
-
     # analyzing the files
     top_file_data = audioowl.analyze_samples(y_top_file, sr)
     bottom_file_data = audioowl.analyze_samples(y_bottom_file, sr)
 
     # find mixing point
     sync_sample, sync_beat_number, sync_beat_accuracy = tu.find_best_sync_point(
-                                                        top_file_beats=top_file_data['beat_samples'],
-                                                        bottom_file_beats=bottom_file_data['beat_samples'],
-                                                        max_mix_sample=y_bottom_file.shape[0],
-                                                        offset=offset,
-                                                        mode=mix_mode)
+        top_file_beats=top_file_data['beat_samples'],
+        bottom_file_beats=bottom_file_data['beat_samples'],
+        max_mix_sample=y_bottom_file.shape[0],
+        offset=offset,
+        mode=mix_mode)
 
-
-    #calculate the timestamp
+    # calculate the timestamp
     sync_time_ms = sync_sample / sr * 1000
-
 
     return sync_time_ms
 
-def get_timestamp_loop(s1, s2, sr=22050, mix_mode='random', offset=880, trim_silence=False,):
 
+def get_timestamp_loop(prev_song, next_song, sr=22050, mix_mode='random', offset=880, trim_silence=False):
+    """
+        :param next_song: next song in the mix
+        :param prev_song: previous song
+        :param sr: sample rate
+        :param mix_mode:
+        :param offset:
+        :param trim_silence:
+        :return: timestamp in milliseconds where the best place to transition is
+    """
     # loading top file
-    y_top_file, sr = librosa.load(s1.filename, sr=sr)
+    top_file = next_song.filename
+    bottom_file = prev_song.filename
+    y_top_file, sr = librosa.load(top_file, sr=sr)
 
     if trim_silence:
         try:
@@ -77,24 +83,23 @@ def get_timestamp_loop(s1, s2, sr=22050, mix_mode='random', offset=880, trim_sil
             # trimming only the leading silence
             y_top_file = y_top_file[i[0]:]
         except:
-            print ('[MixingBear] Failed to trim leading silence')
+            print('[MixingBear] Failed to trim leading silence')
             pass
 
     # loading bottom file
-    y_bottom_file, sr = librosa.load(s2.filename, sr=sr)
+    y_bottom_file, sr = librosa.load(bottom_file, sr=sr)
 
     # checking if the durations allow proper mixing
     y_bottom_file_repetitions = 1
-    while (y_bottom_file.shape[0] * y_bottom_file_repetitions) < y_top_file.shape[0] :
-        y_bottom_file_repetitions+=1
+    while (y_bottom_file.shape[0] * y_bottom_file_repetitions) < y_top_file.shape[0]:
+        y_bottom_file_repetitions += 1
 
     # repeating y_bottom_file if needed
-    if (y_bottom_file_repetitions > 1):
+    if y_bottom_file_repetitions > 1:
         y_bottom_file_duplications = []
         for i in range(y_bottom_file_repetitions):
             y_bottom_file_duplications.append(y_bottom_file)
         y_bottom_file = np.hstack((y_bottom_file_duplications))
-
 
     # analyzing the files
     top_file_data = audioowl.analyze_samples(y_top_file, sr)
@@ -102,14 +107,17 @@ def get_timestamp_loop(s1, s2, sr=22050, mix_mode='random', offset=880, trim_sil
 
     # find mixing point
     sync_sample, sync_beat_number, sync_beat_accuracy = tu.find_best_sync_point(
-                                                        top_file_beats=top_file_data['beat_samples'],
-                                                        bottom_file_beats=bottom_file_data['beat_samples'],
-                                                        max_mix_sample=y_bottom_file.shape[0],
-                                                        offset=offset,
-                                                        mode=mix_mode)
-
+        top_file_beats=top_file_data['beat_samples'],
+        bottom_file_beats=bottom_file_data['beat_samples'],
+        max_mix_sample=y_bottom_file.shape[0],
+        offset=offset,
+        mode=mix_mode)
 
     # mix the files
     sync_time_ms = sync_sample / sr * 1000
 
-    return (sync_time_ms - ((4/(s1.bpm()/60))*1000))
+    return (sync_time_ms, sync_time_ms + ((4 / (prev_song.bpm / 60)) * 1000))
+
+def get_timestamp_loop_in(next_song):
+    #tuple containing the beginning of the song and a timestamp one bar later
+    return (0, (4 / (next_song.bpm / 60)) * 1000)
